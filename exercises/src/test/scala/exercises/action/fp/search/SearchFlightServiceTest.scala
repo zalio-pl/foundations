c22c3c7e024a2c6e508f2f40976a164297039e94
zalio-pl <lzalewski@zalio.pl>
2024-08-03 08:37:38 +0200

    CH03 - refactor test to property based test

--- exercises/src/test/scala/exercises/action/fp/search/SearchFlightServiceTest.scala
+++ exercises/src/test/scala/exercises/action/fp/search/SearchFlightServiceTest.scala
@@ -37,18 +37,12 @@ class SearchFlightServiceTest extends AnyFunSuite with ScalaCheckDrivenPropertyC
   }
 
   test("fromTwoClients should handle errors") {
-    val now   = Instant.now()
-    val today = LocalDate.now()
+    forAll(airportGen, airportGen, dateGen, clientGen, clientGen) { (from, to, date, client1, client2) =>
+      val service = SearchFlightService.fromTwoClients(client1, client2)
+      val result  = service.search(from, to, date).attempt.unsafeRun()
 
-    val flight1 = Flight("1", "BA", parisOrly, londonGatwick, now, Duration.ofMinutes(100), 0, 89.5, "")
-
-    val client1 = SearchFlightClient.constant(IO(List(flight1)))
-    val client2 = SearchFlightClient.constant(IO.fail(new IllegalStateException("Boom!")))
-
-    val service = SearchFlightService.fromTwoClients(client1, client2)
-    val result  = service.search(parisOrly, londonGatwick, today).attempt.unsafeRun()
-
-    assert(result.isSuccess)
+      assert(result.isSuccess)
+    }
   }
 
 }
